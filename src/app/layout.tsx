8fe4e24ddc97381f44b6e9c228ad9815d54bce5e
Chang Wei <changwei1006@gmail.com>
2024-06-28 05:13:55 +0800

    feature/2024-06-27: add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GPVC7Z21XH"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-GPVC7Z21XH');
+        `}} />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

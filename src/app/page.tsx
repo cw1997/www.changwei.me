d373f575315105a8d7781311d517f67b13b6864e
Chang Wei <changwei1006@gmail.com>
2024-06-30 04:51:50 +0800

    add components/ExperienceSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
+import {ExperienceSection} from "@/components/ExperienceSection/ExperienceSection";
 import {Footer} from "@/components/Footer/Footer";
 import {ProfileSection} from "@/components/ProfileSection/ProfileSection";
 import {SkillSection} from "@/components/SkillSection/SkillSection";
+import {Space} from "antd";
 import styles from "./page.module.sass";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
+      <Space className={styles.main} direction={'vertical'} size={48}>
         <ProfileSection />
-        
-        <div className={styles.skill}>
-          <SkillSection />
-        </div>
-      </main>
+        <SkillSection />
+        <ExperienceSection />
+      </Space>
       
       <Footer />
     </div>

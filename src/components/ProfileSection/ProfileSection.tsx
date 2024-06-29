aa180de6232c4b400a27a03cd2e534c1d888f689
Chang Wei <changwei1006@gmail.com>
2024-06-29 23:21:00 +0800

    update components/ProfileSection: fix style, extract data, add language

--- src/components/ProfileSection/ProfileSection.tsx
+++ src/components/ProfileSection/ProfileSection.tsx
@@ -5,6 +5,71 @@ import {Divider, Space, Tag} from "antd";
 import React from "react";
 import styles from "./ProfileSection.module.sass";
 
+const profile = [
+  {
+    key: 'Birthday',
+    value: '1997/10/06'
+  },
+  {
+    key: 'Location',
+    value: (
+      <OutsideLink href={'https://www.google.com/maps/place/Da%E2%80%99an+District,+Taipei+City,+106/@25.026306,121.5232035,14z/data=!3m1!4b1!4m6!3m5!1s0x3442aa2c1969f84d:0x6ea0b5cbf2d9955d!8m2!3d25.0249441!4d121.5433783!16zL20vMDJfNDY3?entry=ttu'}>
+        Taiwan (+886), Taipei City (106), Da’an District
+      </OutsideLink>
+    ),
+  },
+  {
+    key: 'Homeplace',
+    value: (
+      <OutsideLink href={'https://map.baidu.com/search/%E5%90%89%E5%B7%9E%E5%8C%BA/@12789893.52945679,3124670.185,12.7z?querytype=s&da_src=shareurl&wd=%E5%90%89%E5%B7%9E%E5%8C%BA&c=9002&src=0&wd2=%E5%90%89%E5%AE%89%E5%B8%82%E5%90%89%E5%B7%9E%E5%8C%BA&pn=0&sug=1&l=15&b=(13523606.739607658,2856687.76226769;13540382.739607658,2863967.76226769)&from=webmap&biz_forward=%7B%22scaler%22:1,%22styles%22:%22pl%22%7D&sug_forward=fb92f2f75515957842afe6c6&device_ratio=1'}>
+        {"Chinese mainland (+86), JiangXi Province, Ji'an City (343000), Ji’zhou District"}
+      </OutsideLink>
+    ),
+  },
+  {
+    key: 'Major',
+    value: (
+      <>Electrical and Computer Engineering</>
+    ),
+  },
+  {
+    key: 'School',
+    value: (
+      <OutsideLink href={'https://www.ntust.edu.tw/'}>
+        NTUST (National Taiwan University of Science and Technology, Taiwan Tech)
+      </OutsideLink>
+    ),
+  },
+  {
+    key: 'Work',
+    value: (
+      <>
+        <OutsideLink href={'https://risingwave.com'}>
+          RisingWave Labs
+        </OutsideLink> (Web Front-End Engineer)
+      </>
+    ),
+  },
+  {
+    key: 'Language',
+    value: (
+      <Space direction={'vertical'} wrap>
+        <div>
+          <Tag>zh-Hans-CN</Tag>
+          <span>Chinese (Simplified Han) 简体中文（中国大陆）</span>
+        </div>
+        <div>
+          <Tag>zh-Hant-TW</Tag>
+          <span>Chinese (Traditional Han) 繁體中文/正體中文（台灣）</span>
+        </div>
+        <div>
+          <Tag>en</Tag>
+          <span>English</span>
+        </div>
+      </Space>
+    ),
+  },
+]
 
 export interface IPropsSkillSection {
 }
@@ -34,46 +99,12 @@ export const ProfileSection: React.FunctionComponent<IPropsSkillSection> = (prop
         </Space>
         <table className={styles.profile_info_list}>
           <tbody>
-            <tr className={styles.profile_info_list_item}>
-              <td className={styles.profile_info_list_item_key}>Birthday:</td>
-              <td className={styles.profile_info_list_item_value}>1997/10/06</td>
-            </tr>
-            <tr className={styles.profile_info_list_item}>
-              <td className={styles.profile_info_list_item_key}>Location:</td>
-              <td className={styles.profile_info_list_item_value}>
-                <OutsideLink href={'https://www.google.com/maps/place/Da%E2%80%99an+District,+Taipei+City,+106/@25.026306,121.5232035,14z/data=!3m1!4b1!4m6!3m5!1s0x3442aa2c1969f84d:0x6ea0b5cbf2d9955d!8m2!3d25.0249441!4d121.5433783!16zL20vMDJfNDY3?entry=ttu'}>
-                  Taiwan (+886), Taipei City (106), Da’an District
-                </OutsideLink>
-              </td>
-            </tr>
-            <tr className={styles.profile_info_list_item}>
-              <td className={styles.profile_info_list_item_key}>Homeplace:</td>
-              <td className={styles.profile_info_list_item_value}>
-                <OutsideLink href={'https://map.baidu.com/search/%E5%90%89%E5%B7%9E%E5%8C%BA/@12789893.52945679,3124670.185,12.7z?querytype=s&da_src=shareurl&wd=%E5%90%89%E5%B7%9E%E5%8C%BA&c=9002&src=0&wd2=%E5%90%89%E5%AE%89%E5%B8%82%E5%90%89%E5%B7%9E%E5%8C%BA&pn=0&sug=1&l=15&b=(13523606.739607658,2856687.76226769;13540382.739607658,2863967.76226769)&from=webmap&biz_forward=%7B%22scaler%22:1,%22styles%22:%22pl%22%7D&sug_forward=fb92f2f75515957842afe6c6&device_ratio=1'}>
-                    {"Chinese mainland (+86), JiangXi Province, Ji'an City (343000), Ji’zhou District"}
-                </OutsideLink>
-              </td>
-            </tr>
-            <tr className={styles.profile_info_list_item}>
-              <td className={styles.profile_info_list_item_key}>Major:</td>
-              <td className={styles.profile_info_list_item_value}>Electrical and Computer Engineering</td>
-            </tr>
-            <tr className={styles.profile_info_list_item}>
-              <td className={styles.profile_info_list_item_key}>School:</td>
-              <td className={styles.profile_info_list_item_value}>
-                <OutsideLink href={'https://www.ntust.edu.tw/'}>
-                  NTUST (National Taiwan University of Science and Technology, Taiwan Tech)
-                </OutsideLink>
-              </td>
-            </tr>
-            <tr className={styles.profile_info_list_item}>
-              <td className={styles.profile_info_list_item_key}>Work:</td>
-              <td className={styles.profile_info_list_item_value}>
-                <OutsideLink href={'https://risingwave.com'}>
-                  RisingWave Labs
-                </OutsideLink> (Web Front-End Engineer)
-              </td>
-            </tr>
+            {profile.map((item) => (
+              <tr key={item.key} className={styles.profile_info_list_item}>
+                <td className={styles.profile_info_list_item_key}>{item.key}:</td>
+                <td className={styles.profile_info_list_item_value}>{item.value}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
         {/*<div className={styles.profile_info_intro}>*/}
@@ -171,7 +202,11 @@ export const ProfileSection: React.FunctionComponent<IPropsSkillSection> = (prop
       
       <div className={styles.profile_photo}>
         <img className={styles.profile_photo_image} src={photo_image.src} alt={'Chang Wei at Google Taipei office'}/>
-        <div>
+        <div className={styles.profile_photo_description}>
+          Chang Wei attended to the event {'<'}GDG Cloud Taipei AI Techtalk: Meet Google Cloud DevRel Team{'>'} at Google Taipei office. <br/>
+          {/*This photo was taken on Tuesday, June 11, at the Taipei 101 14F (visitor center of Google Taipei office)*/}
+        </div>
+        <div className={styles.profile_photo_info}>
           <table className={styles.profile_photo_info_list}>
             <tbody>
             <tr className={styles.profile_photo_info_list_item}>
@@ -182,7 +217,7 @@ export const ProfileSection: React.FunctionComponent<IPropsSkillSection> = (prop
               <td className={styles.profile_photo_info_list_item_key}>Location:</td>
               <td className={styles.profile_photo_info_list_item_value}>
                 <OutsideLink href={'https://www.google.com/maps/place/Da%E2%80%99an+District,+Taipei+City,+106/@25.026306,121.5232035,14z/data=!3m1!4b1!4m6!3m5!1s0x3442aa2c1969f84d:0x6ea0b5cbf2d9955d!8m2!3d25.0249441!4d121.5433783!16zL20vMDJfNDY3?entry=ttu'}>
-                  Taiwan (+886), Taipei City (106), Da’an District
+                  Taiwan (+886), Taipei City (106), Xinyi District, <br/>Taipei 101 14F (visitor center of Google Taipei office) <br/>(25.033487,121.564922)
                 </OutsideLink>
               </td>
             </tr>

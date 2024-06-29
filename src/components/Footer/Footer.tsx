5fb59e10bbcc61dc88c760b8796c1befd22b5dc0
Chang Wei <changwei1006@gmail.com>
2024-06-29 15:35:14 +0800

    update components/Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
-import {OutsideLink} from "@/components/OutsideLink";
-import {Space} from "antd";
+import {Divider, Space} from "antd";
 import React from "react";
 import Image from "next/image";
 
+import {OutsideLink} from "@/components/OutsideLink";
 import next_logo from '@/assets/images/logo/frontend/next.svg'
 import vercel_logo from '@/assets/images/logo/frontend/vercel.svg'
+import cloudflare_logo from '@/assets/images/logo/Cloudflare_Logo.svg'
 
 import styles from "./Footer.module.sass";
 
@@ -12,33 +13,85 @@ export interface IPropsFooter {
 }
 
 export const Footer: React.FunctionComponent<IPropsFooter> = (props) => {
+  const year = new Date().getFullYear()
+  
   return (
     <footer className={styles.container}>
       <div className={styles.content}>
-        <Space align={'center'}>
-          Powered By
-          <OutsideLink className={styles.logo} href={'https://vercel.com'}>
-            <Image
-              src={vercel_logo}
-              alt="Vercel Logo"
-              className={styles.logo_img}
-              // width={100}
-              height={16}
-              priority
-            />
-          </OutsideLink>
-          And
-          <OutsideLink className={styles.logo} href={'https://nextjs.org/'}>
-            <Image
-              src={next_logo}
-              alt="NextJS Logo"
-              className={styles.logo_img}
-              // width={100}
-              height={16}
-              priority
-            />
-          </OutsideLink>
-        </Space>
+        <div className={styles.text}>
+          <p className={styles.copyright}>
+            © {year}, Chang Wei, All rights reserved. <br/>
+            © {year}, 昌维 版权所有.
+          </p>
+          <div className={styles.nerd_info}>
+            <p>Source code [<OutsideLink href={'https://github.com/cw1997/www.changwei.me'}>cw1997/www.changwei.me</OutsideLink>] is under <OutsideLink href={'https://www.apache.org/licenses/LICENSE-2.0'}>Apache License, Version 2.0 (Apache 2.0)</OutsideLink></p>
+            <p className={styles.copyright_github}>
+              <span>GitHub Repository: <OutsideLink href={'https://github.com/cw1997/www.changwei.me'}/></span>
+            </p>
+            <p>
+              <Space wrap>
+                <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/cw1997/www.changwei.me"/>
+                <img alt="GitHub Repo watchers" src="https://img.shields.io/github/watchers/cw1997/www.changwei.me"/>
+                <img alt="GitHub Repo forks" src="https://img.shields.io/github/forks/cw1997/www.changwei.me"/>
+                <img alt="GitHub branch status" src="https://img.shields.io/github/checks-status/cw1997/www.changwei.me/main"/>
+                <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/cw1997/www.changwei.me?style=flat"/>
+                <img alt="GitHub Created At" src="https://img.shields.io/github/created-at/cw1997/www.changwei.me"/>
+                <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/cw1997/www.changwei.me"/>
+              </Space>
+            </p>
+            <p>
+              <Space wrap>
+                <img alt="GitHub followers" src="https://img.shields.io/github/followers/cw1997"/>
+                <img alt="GitHub User's stars" src="https://img.shields.io/github/stars/cw1997"/>
+                <img alt="X (formerly Twitter) Follow" src="https://img.shields.io/twitter/follow/changwei1006"/>
+              </Space>
+            </p>
+          </div>
+        </div>
+        <Divider/>
+        <div className={styles.powered_by}>
+          <Space align={'center'} className={styles.powered_by_list} split={<Divider type={'vertical'}/>}>
+            <div className={styles.powered_by_item}>
+              Powered by
+              <OutsideLink className={styles.logo} href={'https://nextjs.org/'}>
+                <Image
+                  src={next_logo}
+                  alt="NextJS Logo"
+                  className={styles.logo_img}
+                  // width={100}
+                  height={16}
+                  priority
+                />
+              </OutsideLink>
+            </div>
+            <div className={styles.powered_by_item}>
+              Hosted on
+              <OutsideLink className={styles.logo} href={'https://vercel.com'}>
+                <Image
+                  src={vercel_logo}
+                  alt="Vercel Logo"
+                  className={styles.logo_img}
+                  // width={100}
+                  height={16}
+                  priority
+                />
+              </OutsideLink>
+            </div>
+            <div className={styles.powered_by_item}>
+              CDN:
+              <OutsideLink className={styles.logo} href={'https://cloudflare.com'}>
+                <Image
+                  src={cloudflare_logo}
+                  alt="CloudFlare Logo"
+                  className={styles.logo_img}
+                  // width={100}
+                  height={32}
+                  priority
+                />
+              </OutsideLink>
+            </div>
+          </Space>
+        </div>
       </div>
     </footer>
   )

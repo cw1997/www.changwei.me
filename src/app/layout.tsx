368e5cdd47bf14295d8b40559895540bbf5e851e
Chang Wei <changwei1006@gmail.com>
2024-07-01 04:56:23 +0800

    update app/layout.tsx: using '@next/third-parties/google' to inject google scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import {AntdRegistry} from "@ant-design/nextjs-registry";
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import "@/app/globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,22 +29,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/*<!-- Google tag (gtag.js) -->*/}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GPVC7Z21XH"></script>
-        <script dangerouslySetInnerHTML={{__html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-GPVC7Z21XH');
-        `.split('\n').map(t => t.trim()).join('')}} />
-      </head>
+      {/*<head>*/}
+      {/*  /!*<!-- Google tag (gtag.js) -->*!/*/}
+      {/*  <script async src="https://www.googletagmanager.com/gtag/js?id=G-GPVC7Z21XH"></script>*/}
+      {/*  <script dangerouslySetInnerHTML={{__html: `*/}
+      {/*    window.dataLayer = window.dataLayer || [];*/}
+      {/*    function gtag(){dataLayer.push(arguments);}*/}
+      {/*    gtag('js', new Date());*/}
+      {/*  */}
+      {/*    gtag('config', 'G-GPVC7Z21XH');*/}
+      {/*  `.split('\n').map(t => t.trim()).join('')}} />*/}
+      {/*</head>*/}
+      <GoogleTagManager gtmId="G-GPVC7Z21XH" />
       <body className={inter.className}>
         <AntdRegistry>
           {children}
         </AntdRegistry>
       </body>
+      <GoogleAnalytics gaId={'G-GPVC7Z21XH'} />
     </html>
   );
 }

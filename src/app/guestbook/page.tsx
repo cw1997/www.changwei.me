6d0dd7c6de4b02eb80b79528ed7c472217ec4f86
Chang Wei <changwei1006@gmail.com>
2024-07-07 05:23:47 +0800

    feature/2024-07-07-guestbook: update header

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -4,12 +4,11 @@ import styles from "./page.module.sass";
 
 export default function GuestbookPage() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Guestbook 留言板</h1>
-        <Divider />
-        <GuestbookInput />
-      </main>
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Guestbook 留言板</h1>
+      <Divider/>
+      <GuestbookInput/>
+      <Divider/>
+    </main>
   )
 }

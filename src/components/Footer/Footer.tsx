f2123eaf95d15a4cdb23d959afc5e8fec32a402a
Chang Wei <changwei1006@gmail.com>
2024-07-19 22:56:03 +0800

    update components/Footer: fix html tag to comply with Web HTML semantic

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Divider, Space} from "antd";
 import Image from "next/image";
+import {Divider, Space} from "antd";
+import {CodeOutlined, GithubOutlined} from "@ant-design/icons";
 
 import {OutsideLink} from "@/components/OutsideLink";
 import next_logo from '@/assets/images/logo/frontend/next.svg'
@@ -24,28 +25,32 @@ export const Footer: React.FunctionComponent<IPropsFooter> = (props) => {
             © {year}, 昌维 版权所有.
           </p>
           <div className={styles.nerd_info}>
-            <p>Source code [<OutsideLink href={'https://github.com/cw1997/www.changwei.me'}>cw1997/www.changwei.me</OutsideLink>] is under <OutsideLink href={'https://www.apache.org/licenses/LICENSE-2.0'}>Apache License, Version 2.0 (Apache 2.0)</OutsideLink></p>
+            <p>
+              <CodeOutlined /> Source code [<OutsideLink href={'https://github.com/cw1997/www.changwei.me'}>cw1997/www.changwei.me</OutsideLink>] is under <OutsideLink href={'https://www.apache.org/licenses/LICENSE-2.0'}>Apache License, Version 2.0 (Apache 2.0)</OutsideLink>
+            </p>
             <p className={styles.copyright_github}>
-              <span>GitHub Repository: <OutsideLink href={'https://github.com/cw1997/www.changwei.me'}/></span>
+              <GithubOutlined /> GitHub Repository: <OutsideLink href={'https://github.com/cw1997/www.changwei.me'}/>
             </p>
-            <p>
-              <Space wrap>
-                <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/cw1997/www.changwei.me"/>
-                <img alt="GitHub Repo watchers" src="https://img.shields.io/github/watchers/cw1997/www.changwei.me"/>
-                <img alt="GitHub Repo forks" src="https://img.shields.io/github/forks/cw1997/www.changwei.me"/>
-                <img alt="GitHub branch status" src="https://img.shields.io/github/checks-status/cw1997/www.changwei.me/main"/>
-                <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/cw1997/www.changwei.me?style=flat"/>
-                <img alt="GitHub Created At" src="https://img.shields.io/github/created-at/cw1997/www.changwei.me"/>
-                <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/cw1997/www.changwei.me"/>
-              </Space>
-            </p>
-            <p>
-              <Space wrap>
-                <img alt="GitHub followers" src="https://img.shields.io/github/followers/cw1997"/>
-                <img alt="GitHub User's stars" src="https://img.shields.io/github/stars/cw1997"/>
-                <img alt="X (formerly Twitter) Follow" src="https://img.shields.io/twitter/follow/changwei1006"/>
-              </Space>
-            </p>
+            <div style={{marginTop: 8}}>
+              <div>
+                <Space wrap>
+                  <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/cw1997/www.changwei.me"/>
+                  <img alt="GitHub Repo watchers" src="https://img.shields.io/github/watchers/cw1997/www.changwei.me"/>
+                  <img alt="GitHub Repo forks" src="https://img.shields.io/github/forks/cw1997/www.changwei.me"/>
+                  <img alt="GitHub branch status" src="https://img.shields.io/github/checks-status/cw1997/www.changwei.me/main"/>
+                  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/cw1997/www.changwei.me?style=flat"/>
+                  <img alt="GitHub Created At" src="https://img.shields.io/github/created-at/cw1997/www.changwei.me"/>
+                  <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/cw1997/www.changwei.me"/>
+                </Space>
+              </div>
+              <div>
+                <Space wrap>
+                  <img alt="GitHub followers" src="https://img.shields.io/github/followers/cw1997"/>
+                  <img alt="GitHub User's stars" src="https://img.shields.io/github/stars/cw1997"/>
+                  <img alt="X (formerly Twitter) Follow" src="https://img.shields.io/twitter/follow/changwei1006"/>
+                </Space>
+              </div>
+            </div>
           </div>
         </div>
         <Divider/>

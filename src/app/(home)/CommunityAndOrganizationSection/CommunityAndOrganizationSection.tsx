7d456706248ee00376496277a253565e1d12ff0b
Chang Wei <changwei1006@gmail.com>
2024-09-23 22:10:46 +0800

    add src/(home)/CommunityAndOrganizationSection: add section "CommunityAndOrganizationSection" to homepage

---
+++ src/app/(home)/CommunityAndOrganizationSection/CommunityAndOrganizationSection.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import {Col, Row, Space} from "antd"
+import React from "react"
+import Image from "next/image"
+
+import styles from "./CommunityAndOrganizationSection.module.sass"
+import ntu_gdsc_logo_image from "@/assets/images/logo/ntu_gdsc_logo_with_padding.png"
+import ntust_gdsc_logo_image from "@/assets/images/logo/ntust_gdsc.jpg"
+import ntust_student_council_logo_image from "@/assets/images/logo/ntust_student_council.jpg"
+import risingwave_logo_image from "@/assets/images/logo/risingwave.png"
+import ntust_ece_logo_image from "@/assets/images/logo/ntust_ece.png"
+import ntust_piano_club_logo_image from "@/assets/images/logo/ntust_piano_club.jpg"
+
+const logo_size = 64
+
+const data: {
+  icon: React.ReactNode
+  name: string
+  role: string
+  // contact: string
+  url?: string
+  description?: React.ReactNode
+  // qrcode_image_url?: string
+}[] = [
+  {
+    icon: <Image src={ntu_gdsc_logo_image.src} alt={""} width={logo_size} height={logo_size} />,
+    name: "Google Developer Groups on Campus (National Taiwan University)",
+    role: "Speaker (Efficiency Engineering & Devtools & DevOps)"
+  },
+  {
+    icon: <Image src={ntust_gdsc_logo_image.src} alt={""} width={logo_size} height={logo_size} />,
+    name: "Google Developer Groups on Campus (National Taiwan University of Science and Technology)",
+    role: "Speaker (Web Front-end)"
+  },
+  {
+    icon: <Image src={ntust_student_council_logo_image.src} alt={""} width={logo_size} height={logo_size} />,
+    name: "Student Council (National Taiwan University of Science and Technology)",
+    role: "Vice-President"
+  },
+  {
+    icon: <Image src={risingwave_logo_image.src} alt={""} width={logo_size} height={logo_size} />,
+    name: "RisingWave Labs",
+    role: "Web Front-end Developer (Intern)"
+  },
+  {
+    icon: <Image src={ntust_ece_logo_image.src} alt={""} width={logo_size} height={logo_size} />,
+    name: "Electrical and Computer Engineering (National Taiwan University of Science and Technology)",
+    role: "Master degree"
+  },
+  {
+    icon: <Image src={ntust_piano_club_logo_image.src} alt={""} width={logo_size} height={logo_size} />,
+    name: "Piano club (National Taiwan University of Science and Technology)",
+    role: "Member"
+  },
+] as const
+
+export interface IPropsCommunityAndOrganizationSection {}
+
+export const CommunityAndOrganizationSection: React.FunctionComponent<IPropsCommunityAndOrganizationSection> = (
+  props,
+) => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Community and organization</h2>
+      <Row gutter={[16, 16]} align={"stretch"}>
+        {data.map((item) => (
+          <Col key={item.name} xs={24} sm={24} md={12} lg={8} xl={8} xxl={8}>
+            <div className={styles.item}>
+              <Space align={"start"}>
+                <div className={styles.item_icon}>{item.icon}</div>
+                <div className={styles.item_info}>
+                  <div className={styles.item_info_name}>{item.name}</div>
+                  <div className={styles.item_info_role}>{item.role}</div>
+                </div>
+              </Space>
+              {item.description && (
+                <div className={styles.item_description}>{item.description}</div>
+              )}
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import {Space} from "antd"
 
-import {ContactSection} from "./(home)/ContactSection/ContactSection"
-import {ExperienceSection} from "./(home)/ExperienceSection/ExperienceSection"
 import {ProfileSection} from "./(home)/ProfileSection/ProfileSection"
+import {ContactSection} from "./(home)/ContactSection/ContactSection"
+import {CommunityAndOrganizationSection} from "./(home)/CommunityAndOrganizationSection/CommunityAndOrganizationSection";
 import {SkillSection} from "./(home)/SkillSection/SkillSection"
+import {ExperienceSection} from "./(home)/ExperienceSection/ExperienceSection"
 import styles from "./page.module.sass"
 
 export default function HomePage() {
   return (
     <Space className={styles.main} direction={"vertical"} size={48}>
       <ProfileSection />
+      <CommunityAndOrganizationSection />
       <ContactSection />
       <SkillSection />
       <ExperienceSection />

7c30dbea72bd07a49803d91e3ed707d0ac43650e
Chang Wei <changwei1006@gmail.com>
2024-12-04 14:13:15 +0800

    fix sentry configuration

--- src/app/sentry-example-page/page.tsx
+++ src/app/sentry-example-page/page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://chang-wei.sentry.io/issues/?project=4508343764910080">Issues Page</a>.
+          <a href="https://changwei.sentry.io/issues/?project=4508343764910080">Issues Page</a>.
         </p>
         <p style={{ marginTop: "24px" }}>
           For more information, see{" "}

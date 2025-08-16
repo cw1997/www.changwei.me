71cdead12a07294445defb8c9d163205e16e8797
Chang Wei <changwei1006@gmail.com>
2025-08-16 13:55:00 +0800

    feature/2025-08-14-giscus: setup giscus

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,18 +1,31 @@
-import {GuestbookList} from "@/app/guestbook/GuestbookList/GuestbookList"
+"use client"
+
 import {Divider} from "antd"
 import React from "react"
-import {GuestbookInput} from "./GuestbookInput/GuestbookInput"
 import styles from "./page.module.sass"
+import Giscus from "@giscus/react"
 
 export default function GuestbookPage() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h2 className={styles.title}>Guestbook 留言板</h2>
-        <Divider/>
-        <GuestbookInput/>
-        <Divider/>
-        <GuestbookList/>
+        <Divider />
+        <Giscus
+          id="comments"
+          repo="cw1997/www.changwei.me"
+          repoId="R_kgDOLLaI-Q"
+          category="Announcements"
+          categoryId="DIC_kwDOLLaI-c4Cgdw4"
+          mapping="pathname"
+          // term="Welcome to @giscus/react component!"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme="light"
+          lang="en"
+          loading="lazy"
+        />
       </main>
     </div>
   )

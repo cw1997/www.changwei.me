092484a55f82837b77790e06c58b39c22ea851b6
changwei <changwei1006@gmail.com>
2026-04-01 00:09:43 +0800

    update lint command to use ESLint directly

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from "next";
+import type {NextConfig} from "next"
 import {withSentryConfig} from "@sentry/nextjs"
 
 const nextConfig: NextConfig = {
@@ -23,7 +23,7 @@ const nextConfig: NextConfig = {
     turbopackFileSystemCacheForDev: true,
     // Enable filesystem caching for `next build`
     turbopackFileSystemCacheForBuild: true,
-  }
+  },
 }
 
 export default withSentryConfig(nextConfig, {
@@ -42,11 +42,6 @@ export default withSentryConfig(nextConfig, {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
@@ -55,21 +50,20 @@ export default withSentryConfig(nextConfig, {
 
   // Hides source maps from generated client bundles
   sourcemaps: {
-    deleteSourcemapsAfterUpload: false
+    deleteSourcemapsAfterUpload: false,
   },
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-
-  headers: {
-    key: "Document-Policy",
-    value: "js-profiling",
+  webpack: {
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    automaticVercelMonitors: true,
+    // Automatically annotate React components to show their full name in breadcrumbs and session replay.
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    // Automatically tree-shake Sentry logger statements to reduce bundle size.
+    treeshake: {
+      removeDebugLogging: true,
+    },
   },
 
 })
